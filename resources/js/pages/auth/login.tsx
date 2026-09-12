e7f0e05874d1433e184ae5e0e37520ee86c87296
Opiksurya <gararegew@gmail.com>
2026-09-12 23:02:38 +0700

    Remove public Sign Up link from login page and disable public registration

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -7,7 +7,6 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
-import { register } from '@/routes';
 import { store } from '@/routes/login';
 import { request } from '@/routes/password';
 import PasskeyVerify from '@/components/passkey-verify';
@@ -120,13 +119,6 @@ export default function Login({ status, canResetPassword }: Props) {
                                 </svg>
                                 <span>Sign in with Google</span>
                             </a>
-                        </div>
-
-                        <div className="text-muted-foreground text-center text-sm">
-                            Don't have an account?{' '}
-                            <TextLink href={register()} tabIndex={5}>
-                                Sign up
-                            </TextLink>
                         </div>
                     </>
                 )}
